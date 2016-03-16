8570888a438ce364017bc431b14a7582bfa5dcb5
Daniel Koics <koics.dani@gmail.com>
2016-03-16 14:43:54 +0100

    exposition of CSVDataReader

--- StoreServer_source/server/CSVDataReader.java
+++ StoreServer_source/server/CSVDataReader.java
@@ -1,5 +1,33 @@
 package server;
 
+import java.util.List;
+import java.util.Set;
+
+import datatypes.Person;
+import searching.SearchType;
+
 public class CSVDataReader extends DataReader {
+
+	// instance variables:
+	private String csvFilePath;
+	private List<Person> persons;
+	
+	// constructors:
+	public CSVDataReader(String csvFilePath) {
+		super();
+		this.csvFilePath = csvFilePath;
+	}
+	
+	public CSVDataReader(String searchCriteria, SearchType searchType, String csvFilePath) {
+		super(searchCriteria, searchType);
+		this.csvFilePath = csvFilePath;
+	}
+
+	// searcher:
+	@Override
+	Set<Person> getPersons(String searchCriteria, SearchType searchType) {
+		// TODO search engine comes here
+		return null;
+	}
 
 }
