b919f4b43b49da9e0d602e0e44db72a6dfd5f3fd
Daniel Koics <koics.dani@gmail.com>
2016-03-16 14:26:34 +0100

    abstract DataReader ready

--- StoreServer_source/server/DataReader.java
+++ StoreServer_source/server/DataReader.java
@@ -1,5 +1,47 @@
 package server;
 
-public class DataReader {
+import java.util.Set;
+
+import datatypes.Person;
+import searching.SearchType;
+
+public abstract class DataReader {
+
+	// instance variables:
+	private String searchCriteria;
+	private SearchType searchType;
+
+	// constructors:
+	public DataReader() {
+	}
+
+	public DataReader(String searchCriteria, SearchType searchType) {
+		this.searchCriteria = searchCriteria;
+		this.searchType = searchType;
+	}
+
+	// getters and setters:
+	public String getSearchCriteria() {
+		return searchCriteria;
+	}
+
+	public void setSearchCriteria(String searchCriteria) {
+		this.searchCriteria = searchCriteria;
+	}
+
+	public SearchType getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(SearchType searchType) {
+		this.searchType = searchType;
+	}
+	
+	// abstract of searcher:
+	Set<Person> getPersons() {
+		return getPersons(searchCriteria, searchType);
+	}
+	
+	abstract Set<Person> getPersons(String searchCriteria, SearchType searchType);
 
 }
