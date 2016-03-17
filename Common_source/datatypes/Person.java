71321147bcb6be9e3a0d679e8ee195aa7cf0156b
Daniel Koics <koics.dani@gmail.com>
2016-03-17 14:45:59 +0100

    nullpointer problem solved

--- Common_source/datatypes/Person.java
+++ Common_source/datatypes/Person.java
@@ -1,6 +1,7 @@
 package datatypes;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Person implements Serializable {
@@ -15,6 +16,7 @@ public class Person implements Serializable {
 		super();
 		this.name = name;
 		this.email = email;
+		skillset = new ArrayList<Skill>();
 	}
 	
 	public Person() {
@@ -49,6 +51,37 @@ public class Person implements Serializable {
 	public void addSkill(Skill skill) {
 		// TODO: what if (skillset == null) ?
 		skillset.add(skill);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 	
 }
