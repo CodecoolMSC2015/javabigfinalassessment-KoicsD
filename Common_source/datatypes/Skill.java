93859286951972b7a5d799ed47ea6a5eb71b64d8
Daniel Koics <koics.dani@gmail.com>
2016-03-25 00:08:39 +0100

    unnecessary hashCode and toString Override removed from class Skill

--- Common_source/datatypes/Skill.java
+++ Common_source/datatypes/Skill.java
@@ -33,31 +33,6 @@ public class Skill implements Serializable, HtmlCompatible {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Skill other = (Skill) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toHtmlString() {
 		String asHtml = getName() + "<br/>";
 		asHtml += "&nbsp;&nbsp;Rate: " + getRate() + "<br/>";
